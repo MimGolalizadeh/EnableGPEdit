45c478233ec4e47b02abcbca97a31d73418fae20
Mojtaba Golalizadeh <106627627+MimGolalizadeh@users.noreply.github.com>
2024-04-24 08:14:31 +0200

    Add files via upload

---
+++ EnableGPEdit.bat
@@ -0,0 +1,11 @@
+@echo off
+pushd "%~dp0"
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+pause
+
+:: This script enables Group Policy Editor in Widnwos Home Edition
+:: https://github.com/MimGolalizadeh
